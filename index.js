bdeca103be81490e3a7f79c7790b469ceeb69b88
David Vega <dvega920@gmail.com>
2019-12-22 14:58:08 -0500

    Changed structure of inquirer. Removed switch and added if instead

--- index.js
+++ index.js
@@ -1,77 +1,36 @@
 //Require packages and assign to variables
 const inquirer = require('inquirer');
-const util = require('util');
-// const cTable = require("console.table");
 
 start();
 
 function start() {
-    const questions = inquirer.prompt([
+    inquirer.prompt([
         {
-            type: "list",
             name: "choice",
+            type: "list",
             message: "What do you want to do?",
             choices: [
-                {
-                    name: "Add Department",
-                    value: "Add_Dept",
-                },
-                {
-                    name: "Add Role",
-                    value: "Add_Role"
-                },
-                {
-                    name: "Add Employee",
-                    value: "Add_Emp"
-                },
-                {
-                    name: "View All Department",
-                    value: "View_All_Dept"
-                },
-                {
-                    name: "View Department Salaries (Combined)",
-                    value: "View_Dept_Salaries_Combined"
-                },
-                {
-                    name: "View All Roles",
-                    value: "View_All_Roles"
-                },
-                {
-                    name: "View All Employees",
-                    value: "View_All_Emp"
-                },
-                {
-                    name: "View Employees By Manager",
-                    value: "View_Emp_By_Mgr"
-                },
-                {
-                    name: "Update Employee Roles",
-                    value: "Updated_Emp_Roles"
-                },
-                {
-                    name: "Update Employee Manager",
-                    value: "Update_Emp_Mgr"
-                },
-                {
-                    name: "Delete Departement",
-                    value: "Delete_Dept"
-                },
-                {
-                    name: "Delete Role",
-                    value: "Delete_Role"
-                },
-                {
-                    name: "Delete Employee",
-                    value: "Delete_Emp"
-                },
-                {
-                    name: "Exit",
-                    value: "exit"
-                }
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "View All Department",
+                "View Department Salaries (Combined)",
+                "View All Roles",
+                "View All Employees",
+                "View Employees By Manager",
+                "Update Employee Roles",
+                "Update Employee Manager",
+                "Delete Departement",
+                "Delete Role",
+                "Delete Employee",
+                "Exit",
             ]
         }
 
-    ]).then(answers => {
-        console.table(answers)
+    ]).then(answer => {
+        if (answer.choice === "Add Employee") {
+            console.log("You selected Add Employee");
+        }
+
     })
 }
