48ed541493fa12121176c49ce8bbe924a7389c8e
David Vega <dvega920@gmail.com>
2019-12-22 21:54:47 -0500

    added comments, renamed query to search for all employees. added quit() to end connection

--- db/index.js
+++ db/index.js
@@ -5,10 +5,16 @@ class DATABASE {
         this.connection = connection;
     }
 
-    findAllEmployees() {
+    queryAllEmployees() {
         return this.connection.query(
             "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
         );
+    }
+
+    //ends the connection when user selects 'Exit'.  Will need to restart server again to run addt'l queries.
+    quit() {
+        console.log("Thank you and Goodbye!");
+        connection.end();
     }
 }
 module.exports = new DATABASE(connection);
